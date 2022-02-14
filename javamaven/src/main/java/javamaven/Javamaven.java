0122ef40739abd60e24861696959654037426d55
Varnit <varnitgupta16@gmail.com>
2022-02-14 15:53:26 +0530

    commit

---
+++ javamaven/src/main/java/javamaven/Javamaven.java
@@ -0,0 +1,10 @@
+package javamaven;
+
+public class Javamaven {
+
+	public static void main(String[] args) {
+		System.out.println("hello world by varnit");
+		// TODO Auto-generated method stub
+
+	}
+}
